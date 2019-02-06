1be4208281e850e5d763cf3cb0a1f5910d7527d8
LuisSumpango <lsfelipeayu@gmail.com>
2019-02-05 20:20:56 -0600

    Mi segunda vez

--- Tarea1/src/texto/Ejercicio5.java
+++ Tarea1/src/texto/Ejercicio5.java
@@ -5,6 +5,8 @@
  */
 package texto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luis
@@ -16,6 +18,36 @@ public class Ejercicio5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner ingresar = new Scanner (System.in);
+        String producto = "";
+        float total = 0;
+        float precio = 0;
+        
+        while (precio >= 0)
+        {
+            System.out.println("Ingrese el nombre del producto:.");
+            producto = ingresar.next();
+            System.out.println("Ingrese el precio del producto:.");
+            precio = ingresar.nextFloat();
+            
+            if (precio >= 1)
+            {
+                total = (total+precio);
+            }
+                
+        }
+        System.out.println("El total es de:"+ total);
+        if (total >= 1500)
+        {
+            System.out.println("Con el descuento el total es de:."+ (total*0.1));    
+        }
+        else if (total >= 500)
+        {
+            System.out.println("Con el descuento el total es de:."+ (total*0.05));
+        }
+        
+        
+        
     }
     
 }
